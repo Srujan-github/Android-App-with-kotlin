1ebe243da5958dc2ad9c5f13aa254d65fb7e8b60
Srujan-github <thammishettisaisrujan123@gmail.com>
2024-09-18 21:50:55 +0530

    added button and Toast

--- app/src/main/java/learning/kotlin/android_course/MainActivity.kt
+++ app/src/main/java/learning/kotlin/android_course/MainActivity.kt
@@ -1,6 +1,7 @@
 package learning.kotlin.android_course
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import learning.kotlin.android_course.databinding.ActivityMainBinding
 
@@ -12,7 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.tvFirstText.text = "Hello Android!"
+        binding.btnRoll.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
