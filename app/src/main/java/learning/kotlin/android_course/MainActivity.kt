930f8ee4c05c1ce7ad7371e3786cc26550c93f49
Srujan-github <thammishettisaisrujan123@gmail.com>
2024-11-10 20:34:08 +0530

    completed the dic project

--- app/src/main/java/learning/kotlin/android_course/MainActivity.kt
+++ app/src/main/java/learning/kotlin/android_course/MainActivity.kt
@@ -13,14 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnRoll.setOnClickListener {
+        binding.rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
         val randomNumber = Random.nextInt(6) + 1
-         binding.tvFirstText.text = getString(R.string.roll_dice, randomNumber)
+        val drawableResource = when(randomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+         binding.diceImage.setImageResource(drawableResource)
     }
 
 }
