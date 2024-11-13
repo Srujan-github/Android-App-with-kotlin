02d140efc9351bc4746430645848ce7832279d37
Srujan-github <thammishettisaisrujan123@gmail.com>
2024-11-13 21:20:43 +0530

    Added button click listener and used FindByID

--- app/src/main/java/learning/kotlin/android_course/MainActivity.kt
+++ app/src/main/java/learning/kotlin/android_course/MainActivity.kt
@@ -1,17 +1,33 @@
 package learning.kotlin.android_course
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import learning.kotlin.android_course.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+        addNickName(it)
+        }
+    }
+    fun addNickName(view: View){
+        val editText = findViewById<EditText>(R.id.editNickNameText)
+        val nickNameText =  findViewById<TextView>(R.id.nickNameText)
+        nickNameText.text = editText.text
+        nickNameText.visibility = View.VISIBLE
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
 }
