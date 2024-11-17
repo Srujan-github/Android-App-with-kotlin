90d6a5c5f8089bcacfadb90e79f529403aa45891
Srujan-github <thammishettisaisrujan123@gmail.com>
2024-11-17 17:09:52 +0530

    Removed unwanted files and Learned about constraint layout

--- app/src/main/java/learning/kotlin/MyName.kt
+++
@@ -1,6 +0,0 @@
-package learning.kotlin
-
-data class MyName(
-    var name: String = "",
-    var nickName: String = ""
-)
--- app/src/main/java/learning/kotlin/android_course/MainActivity.kt
+++ app/src/main/java/learning/kotlin/android_course/MainActivity.kt
@@ -1,37 +1,14 @@
 package learning.kotlin.android_course
 
-import android.content.Context
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import learning.kotlin.MyName
 import learning.kotlin.android_course.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var binding: ActivityMainBinding
-    private val myName: MyName = MyName("Sai Srujan")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.myName = myName
-        binding.button.setOnClickListener {
-            addNickName(it)
-        }
-    }
+        binding =ActivityMainBinding.inflate(layoutInflater)
 
-    private fun addNickName(view: View) {
-        binding.apply {
-            myName?.nickName = editNickNameText.text.toString()
-            initializeViewTreeOwners()
-            nickNameText.visibility = View.VISIBLE
-            editNickNameText.visibility = View.GONE
-            view.visibility = View.GONE
-        }
-        binding.myName = myName
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
-
 }
